08a71869034d4fbc344a5e4f57f15d10ffae68c7
Jung-Minji <mjjunghp@naver.com>
2020-11-03 16:21:49 +0900

    first commit

---
+++ app/src/main/java/com/example/newfacts/MapItem.java
@@ -0,0 +1,28 @@
+package com.example.newfacts;
+
+public class MapItem {
+    String cafe_name;
+    String distance;
+    //int resId;
+
+    public MapItem(String cafe_name, String distance){
+        this.cafe_name = cafe_name;
+        this.distance = distance;
+    }
+
+    public String getCafe_name() {
+        return cafe_name;
+    }
+
+    public void setCafe_name(String cafe_name) {
+        this.cafe_name = cafe_name;
+    }
+
+    public String getDistance() {
+        return distance;
+    }
+
+    public void setDistance(String distance) {
+        this.distance = distance;
+    }
+}
