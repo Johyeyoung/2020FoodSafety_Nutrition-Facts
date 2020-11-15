1ebbde44d8a6d7e98927700acd06d3c33ec46b8d
Park,Minjeong <55116793+otnaj@users.noreply.github.com>
2020-11-15 18:46:36 +0900

    Update UserInfo.java

--- app/src/main/java/com/example/newfacts/UserInfo.java
+++ app/src/main/java/com/example/newfacts/UserInfo.java
@@ -1,10 +1,12 @@
-package com.newfact.newfacts;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserInfo {
     public String sex = "";
-    public int age = 0;
-    public int weight=0;
-    public int height=0;
+    public String age = "0";
+    public String height="0";
+    public String weight="0";
 
     public boolean milk_allergy=false;
     public boolean soybean_allergy=false;
@@ -14,17 +16,34 @@ public class UserInfo {
 
     public UserInfo(){}
 
-    public UserInfo( String sex, int age, int height, int weight,
+    public UserInfo( String sex, String age, String height, String weight,
                     boolean milk_allergy, boolean soybean_allergy, boolean peach_allergy,
                     boolean tomato_allergy, boolean squid_allergy){
+
         this.sex = sex;
         this.age = age;
-        this.weight = weight;
         this.height = height;
+        this.weight = weight;
         this.milk_allergy = milk_allergy;
         this.soybean_allergy = soybean_allergy;
         this.peach_allergy = peach_allergy;
         this.tomato_allergy = tomato_allergy;
         this.squid_allergy = squid_allergy;
+    }
+
+
+    public Map<String, Object> toMap(){
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("sex", sex);
+        result.put("age", age);
+        result.put("height", height);
+        result.put("weight", weight);
+        result.put("milk_allergy", milk_allergy);
+        result.put("soybean_allergy", soybean_allergy);
+        result.put("peach_allergy", peach_allergy);
+        result.put("tomato_allergy", tomato_allergy);
+        result.put("squid_allergy", squid_allergy);
+
+        return result;
     }
 }
