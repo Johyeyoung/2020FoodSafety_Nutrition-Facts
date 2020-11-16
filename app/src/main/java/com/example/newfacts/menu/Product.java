64083b0f30d2d4d9a35aa9b0832e0e29f9107a9f
dodo486 <47061005+Johyeyoung@users.noreply.github.com>
2020-11-16 19:47:20 +0900

    파이어베이스 연동

--- app/src/main/java/com/example/newfacts/menu/Product.java
+++ app/src/main/java/com/example/newfacts/menu/Product.java
@@ -3,28 +3,47 @@ package com.example.newfacts.menu;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.google.firebase.database.PropertyName;
+
 import java.util.HashMap;
 import java.util.Map;
 
 
 public class Product implements Parcelable{
-    private String title;
-    private String price;
+    private String franchise;
+    private String name;
+    private String eng;
     private String category;
-    private boolean check=false;
+    private String desc;
+    private String pic;
+    private String nutrition;
+    private String allergy;
+    private String volume;
     private String key;
-    public Product(String title, String price, String category) {
-        this.title = title;
-        this.price = price;
+    public Product(String eng, String allergy, String category, String desc, String franchise,
+                   String name, String nutrition, String pic, String volume) {
+        this.franchise = franchise;
+        this.name = name;
+        this.eng = eng;
         this.category = category;
+        this.desc = desc;
+        this.pic = pic;
+        this.nutrition = nutrition;
+        this.allergy = allergy;
+        this.volume = volume;
     }
     public Product(){}
 
     public Product(Parcel in) {
-        title = in.readString();
-        price = in.readString();
+        franchise = in.readString();
+        name = in.readString();
+        eng = in.readString();
         category = in.readString();
-        check = in.readByte() != 0;
+        desc = in.readString();
+        pic = in.readString();
+        nutrition = in.readString();
+        allergy = in.readString();
+        volume = in.readString();
         key = in.readString();
     }
 
@@ -43,55 +62,45 @@ public class Product implements Parcelable{
     public String getKey() {
         return key;
     }
-
     public void setKey(String key) {
         this.key = key;
     }
-
-    public boolean isCheck() {
-        return check;
-    }
-
-    public void setCheck(boolean check) {
-        this.check = check;
-    }
-
     public String getCategory() {
         return category;
     }
-
-    public void setCategory(String category) {
-        this.category = category;
+    public String getFranchise() {
+        return franchise;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDesc() {
+        return desc;
+    }
+    public String getVolume() {
+        return volume;
+    }
+    public String getAllergy() {
+        return allergy;
+    }
+    public String getPic() {
+        return pic;
+    }
+    public String getNutrition() {
+        return nutrition;
     }
 
-    public String getTitle() {
-        return title;
-    }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getPrice() {
-        return price;
-    }
-
-    public void setPrice(String price) {
-        this.price = price;
-    }
 
     @Override
     public String toString() {
-        return "Product{" +
-                "title='" + title + '\'' +
-                ", price='" + price + '\'' +
-                ", category='" + category + '\'' +
-                '}';
+        return  franchise + "-" + name +"-"+ eng +"-" + category + "-"+
+                desc + "-" + nutrition +"-" + allergy + "-" + pic + "-" + volume;
     }
     public Map<String, Object> toMap() {
         HashMap<String, Object> result = new HashMap<>();
-        result.put("title", title);
-        result.put("price", price);
+        result.put("franchise", franchise);
+        result.put("name", name);
         result.put("category", category);
         return result;
     }
@@ -104,10 +113,10 @@ public class Product implements Parcelable{
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(title);
-        dest.writeString(price);
+        dest.writeString(franchise);
+        dest.writeString(name);
         dest.writeString(category);
-        dest.writeByte((byte) (check ? 1 : 0));
+        dest.writeString(desc);
         dest.writeString(key);
     }
 }
