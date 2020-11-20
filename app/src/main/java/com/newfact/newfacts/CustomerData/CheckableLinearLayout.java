f14d85fdeabfb186ea88b879e29e12c5f18bc92e
dodo486 <missenergy@naver.com>
2020-11-20 10:27:40 +0900

    add firebase
     Conflicts:
    	app/src/main/java/com/example/newfacts/ProductDetailPage.java
    	app/src/main/java/com/example/newfacts/SearchListActivity.java
    	app/src/main/java/com/example/newfacts/menu/Product.java
    	app/src/main/res/layout/activity_product_detail_page.xml

---
+++ app/src/main/java/com/newfact/newfacts/CustomerData/CheckableLinearLayout.java
@@ -0,0 +1,38 @@
+package com.newfact.newfacts.CustomerData;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.CheckBox;
+import android.widget.Checkable;
+import android.widget.LinearLayout;
+
+import com.newfact.newfacts.R;
+
+public class CheckableLinearLayout extends LinearLayout implements Checkable {
+
+    public CheckableLinearLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+    @Override
+    public void setChecked(boolean checked) {
+        CheckBox cb = (CheckBox)findViewById(R.id.checkBox1);
+        if(cb.isChecked()!=checked){
+            cb.setChecked(checked);
+        }
+    }
+
+    @Override
+    public boolean isChecked() {
+        CheckBox cb = (CheckBox)findViewById(R.id.checkBox1);
+        return cb.isChecked();
+    }
+
+    @Override
+    public void toggle() {//현재 checked 상태를 바꾸면 UI에 반영
+        CheckBox cb = (CheckBox) findViewById(R.id.checkBox1) ;
+        setChecked(cb.isChecked() ? false : true) ;
+
+    }
+}
---
+++ app/src/main/java/com/newfact/newfacts/menu/UserInfo.java
@@ -0,0 +1,50 @@
+package com.newfact.newfacts.menu;
+
+public class UserInfo {
+
+
+    private static final UserInfo instance = new UserInfo();
+
+    public UserInfo() {
+    }
+
+    public static UserInfo getInstance() {
+        return instance;
+    }
+
+    private String id = null;
+    private String sex = null;
+    private String age = null;
+    private String height = null;
+    private String weight = null;
+
+    private String allergy = null;
+    private String nutrition= null;
+
+    //    private UserInfo(String id, String sex, String age, String height, String weight, String allergy, String nutrition) {
+//        this.id = id;
+//        this.sex = sex;
+//        this.age = age;
+//        this.height = height;
+//        this.weight = weight;
+//        this.allergy = allergy;
+//        this.nutrition = nutrition;
+//    }
+    public void setId(String id){ this.id = id; }
+    public void setSex(String sex){ this.sex = sex; }
+    public void setAge(String age){ this.age = age; }
+    public void setHeight(String height){ this.height = height; }
+    public void setWeight(String weight){ this.weight = weight; }
+    public void setAllergy(String allergy){ this.allergy = allergy;}
+    public void setNutrition(String nutrition){this.nutrition = nutrition;}
+
+
+    public String getId(){return this.id;}
+    public String getSex(){return this.sex;}
+    public String getAge(){return this.age;}
+    public String getHeight(){return this.height;}
+    public String getWeight(){return this.weight;}
+    public String getAllergy(){return this.allergy;}
+    public String getNutrition(){return this.nutrition;}
+
+}
